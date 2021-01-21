5df66fdd1491875e4eb066c471f9ef2da9dea83e
harshjoeyit <harshit02gangwar@gmail.com>
2021-01-21 13:38:23 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
       <h1>Count: {count}</h1>
       <button onClick={() => {setCount(count + 1)}}>Add</button>
       <button onClick={() => {setCount(count - 1)}}>Sub</button>
+      <h3>Hey dude I am the last change</h3>
     </div>
   )
 }
