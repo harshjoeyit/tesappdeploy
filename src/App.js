96bb774bfae73469de052091b456ac327bd82613
harshjoeyit <harshit02gangwar@gmail.com>
2021-01-21 13:16:57 +0530

    minor changes in the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          arshit's App
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={() => {setCount(count + 1)}}>Add</button>
+      <button onClick={() => {setCount(count - 1)}}>Sub</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
